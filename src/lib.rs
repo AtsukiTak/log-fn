0a2f1fcda35cd56762b12096ca743d07e9b85fbb
AtsukiTak <takatomgoo@gmail.com>
2020-05-28 05:21:39 +0900

    attr module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 extern crate proc_macro;
 
+mod attrs;
+
 use proc_macro::TokenStream as StdTokenStream;
 use proc_macro2::TokenStream;
 
 #[proc_macro_attribute]
-pub fn logfn(_attr: StdTokenStream, tokens: StdTokenStream) -> StdTokenStream {
-    tokens
+pub fn logfn(attr: StdTokenStream, input: StdTokenStream) -> StdTokenStream {
+    let attr = syn::parse_macro_input!(attr as syn::AttributeArgs);
+    // let input = syn::parse_macro_input!(input as syn::ItemFn);
+
+    println!("{:?}", attr[0]);
+    input
 }
--- tests/logfn_pre.rs
+++ tests/logfn_pre.rs
@@ -1,6 +1,8 @@
 use logfn::logfn;
 
-#[logfn(pre, post)]
+#[logfn(pre, msg = "", level = "info")]
+#[logfn(post, msg = "", level = "error", if = "Result::is_err")]
+#[logfn(post, msg = "", level = "info", if = "Result::is_ok")]
 fn add(a: usize, b: usize) -> usize {
     a + b
 }
