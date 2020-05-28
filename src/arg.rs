46f89ea4a5ddea00b5f6d61166418d59f7e8909d
AtsukiTak <takatomgoo@gmail.com>
2020-05-28 15:04:02 +0900

    attrs -> arg

---
+++ src/arg.rs
@@ -0,0 +1,85 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Arg {
+    Type(TypeArg),
+    Level(LevelArg),
+    Msg(MsgArg),
+    If(IfArg),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum TypeArg {
+    Pre,
+    Post,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum LevelArg {
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MsgArg {
+    pub msg: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct IfArg {
+    pub path: String,
+}
+
+macro_rules! unrecognized_item {
+    ($item:expr) => {
+        return Err(syn::Error::new_spanned($item, "Unrecognized attribute."));
+    };
+}
+
+pub fn from_input_vec(args: syn::AttributeArgs) -> syn::Result<Vec<Arg>> {
+    args.into_iter().map(from_input).collect()
+}
+
+pub fn from_input(arg: syn::NestedMeta) -> syn::Result<Arg> {
+    let meta = match arg {
+        syn::NestedMeta::Meta(meta) => meta,
+        _ => unrecognized_item!(arg),
+    };
+
+    match meta.clone() {
+        syn::Meta::Path(path) => {
+            if path.is_ident("pre") {
+                Ok(Arg::Type(TypeArg::Pre))
+            } else if path.is_ident("post") {
+                Ok(Arg::Type(TypeArg::Post))
+            } else {
+                unrecognized_item!(path)
+            }
+        }
+        syn::Meta::NameValue(syn::MetaNameValue {
+            path,
+            lit: syn::Lit::Str(s),
+            ..
+        }) => {
+            if path.is_ident("level") {
+                let level = match s.value().as_str() {
+                    "trace" => LevelArg::Trace,
+                    "debug" => LevelArg::Debug,
+                    "info" => LevelArg::Info,
+                    "warn" => LevelArg::Warn,
+                    "error" => LevelArg::Error,
+                    _ => unrecognized_item!(s),
+                };
+                Ok(Arg::Level(level))
+            } else if path.is_ident("msg") {
+                Ok(Arg::Msg(MsgArg { msg: s.value() }))
+            } else if path.is_ident("if") {
+                Ok(Arg::If(IfArg { path: s.value() }))
+            } else {
+                unrecognized_item!(meta)
+            }
+        }
+        _ => unrecognized_item!(meta),
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 extern crate proc_macro;
 
-mod attrs;
+mod arg;
 
 use proc_macro::TokenStream as StdTokenStream;
 use proc_macro2::TokenStream;
 
 #[proc_macro_attribute]
-pub fn logfn(attr: StdTokenStream, input: StdTokenStream) -> StdTokenStream {
-    let attr = syn::parse_macro_input!(attr as syn::AttributeArgs);
+pub fn logfn(input_args: StdTokenStream, input: StdTokenStream) -> StdTokenStream {
+    let input_args = syn::parse_macro_input!(input_args as syn::AttributeArgs);
     // let input = syn::parse_macro_input!(input as syn::ItemFn);
 
-    println!("{:?}", attr[0]);
+    let args = arg::from_input_vec(input_args);
+    println!("{:?}", args);
+
     input
 }
