31b6494c87366871a02dceda6e02fa7ed566637b
AtsukiTak <takatomgoo@gmail.com>
2020-05-28 18:30:20 +0900

    fix tests

--- tests/logfn_pre.rs
+++ tests/logfn_pre.rs
@@ -1,14 +1,13 @@
 use logfn::logfn;
 
 #[logfn(pre, msg = "execute add", level = "info")]
-#[logfn(post, msg = "executed add", level = "error", if = "Result::is_err")]
-#[logfn(post, msg = "executed add", level = "info", if = "Result::is_ok")]
-fn add(a: usize, b: usize) -> usize {
-    a + b
+#[logfn(post, msg = "executed add", level = "error", if = "Option::is_none")]
+#[logfn(post, msg = "executed add", level = "info", if = "Option::is_some")]
+fn add(a: usize, b: usize) -> Option<usize> {
+    a.checked_add(b)
 }
 
 #[logfn(pre, msg = "execute add", level = "info")]
-#[logfn(post, msg = "executed add", level = "error", if = "Result::is_err")]
 fn generics<T>(a: T) -> T
 where
     T: std::fmt::Debug,
@@ -17,12 +16,7 @@ where
 }
 
 #[logfn(pre, msg = "execute async_fn", level = "info")]
-#[logfn(
-    post,
-    msg = "executed async_fn",
-    level = "error",
-    if = "Result::is_err"
-)]
+#[logfn(post, msg = "executed async_fn", level = "error")]
 async fn async_fn(a: usize) -> usize {
     async { a }.await
 }
